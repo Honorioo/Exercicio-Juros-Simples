d68e53d35d0ce665f6daf9df5cde0c5a4074d607
Vinicius <viniciusgomeshonorio@hotmail.com>
2021-03-28 12:42:26 -0300

    Exercicio Juros

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Exercicio_Juros_Simples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double capital, TaxaJros, Tempo, resultadoJ, resultadoM; 
+
+            Console.WriteLine("-------Juros Simples-------");
+
+
+            Console.Write("Capital [c] (R$).......: ");
+            capital = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Taxa de Juros (i).......: ");
+            TaxaJros = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Tempo (t) meses.......: ");
+            Tempo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+
+            resultadoJ = capital * TaxaJros * Tempo;
+            resultadoM = capital + TaxaJros;
+
+                    Console.WriteLine();
+                    Console.WriteLine($"Juros (R$).....: {resultadoJ}");
+                    Console.WriteLine($"Montante (R$).....: {resultadoM}");
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
